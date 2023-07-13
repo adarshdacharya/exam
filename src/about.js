072186d76f36cc565ded7ad08616b00dfd38b7cc
Adarsh D Acharya <adarshacharya1811@gmail.com>
2023-07-13 17:53:17 +0530

    first commit

---
+++ src/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      
+
+      {/* Footer */}
+      <footer className="mt-5 p-3 text-center bg-light">
+        &copy; 2023 Foodie Delights. All rights reserved.
+      </footer>
+    </div>
+  );
+};
+
+export default About;
